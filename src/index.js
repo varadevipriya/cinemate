388b5d87b41d61eaffd511fd405bec40f78a2f91
devipriya <devipriya.akkineni123@gmail.com>
2023-08-25 20:43:35 +0530

    completed files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {ScrollToTop} from './components'
+import { ScrollToTop } from "./components";
+
 import './index.css';
 import App from './App';
 
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-     <ScrollToTop />
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
